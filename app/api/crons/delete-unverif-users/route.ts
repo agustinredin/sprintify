54bb8f5306691de3a52a16eb39e7202b9fa8017b
Agustin Redin <86842705+agustinredin@users.noreply.github.com>
2024-11-08 04:53:05 -0300

    test cron v2

--- app/api/crons/delete-unverif-users/route.ts
+++ app/api/crons/delete-unverif-users/route.ts
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic" // static by default, unless reading the 
 
 export async function GET(request: Request) {
   try {
-    const result = await sql`DELETE FROM users WHERE verified = false`
+    const result = await sql`DELETE * FROM users WHERE verified = false OR verified is NULL`
     console.log(result)
     return new Response(`CRON job executed. Unverified users deleted: ${result.rowCount ?? 0}`)
   } catch (error) {
